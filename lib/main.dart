78367d7b86663b6206a1d3f110433a29f4be46c5
alphadev-MacBook Pro <alphadev32@gmail.com>
2025-10-20 04:03:30 +0300

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'Core/Utils/app.routes.dart';
 import 'Core/Widgets/App_localization.dart';
 import 'feature/splash/views/splash_view.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
